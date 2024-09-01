e14678705a04958bad55999893856304374f6fe4
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-08-31 22:37:58 -0700

    more updates-

--- constants.py
+++ constants.py
@@ -4,6 +4,21 @@ BASE24_OVERFLOW = 24
 BASE31_OVERFLOW = 31
 BASE12_OVERFLOW = 12
 BASE10_OVERFLOW = 10
+BASE5_OVERFLOW = 5
+
+
+DATA_FOLDERPATH = f'{dirname(abspath(__file__))}/screentime-local'
+DATA_FILEPATH = f'{DATA_FOLDERPATH}\\data.json'
+DAYLOG_FILEPATH = f'{DATA_FOLDERPATH}\\daylog.json'
+EXIT_FILEPATH = f'{DATA_FOLDERPATH}\\exit.txt'
+
+START_TIMESTAMP = 'start timestamp'
+CURRENT_TIMESTAMP = 'current timestamp'
+TRACKED_SECONDS = 'tracked seconds'
+TRACKED_MINUTES = 'tracked minutes'
+TRACKED_HOURS = 'tracked hours'
+M30_INTERVAL_TRACKER = '30 minute interval tracker'
+CURRENT_WEEKDAY = 'current weekday'
 
 # SUNDAY = 'sunday'
 # MONDAY = 'monday'
@@ -25,19 +40,3 @@ BASE10_OVERFLOW = 10
 # OCTOBER = 'october'
 # NOVEMBER = 'november'
 # DECEMBER = 'december'
-
-# in the new new reform
-DATA_FOLDERPATH = f'{dirname(abspath(__file__))}/screentime-local'
-DATA_FILEPATH = f'{DATA_FOLDERPATH}\\data.json'
-DAYLOG_FILEPATH = f'{DATA_FOLDERPATH}\\daylog.json'
-EXIT_FILEPATH = f'{DATA_FOLDERPATH}\\exit.txt'
-
-START_TIMESTAMP = 'start timestamp'
-CURRENT_TIMESTAMP = 'current timestamp'
-TRACKED_SECONDS = 'tracked seconds'
-TRACKED_MINUTES = 'tracked minutes'
-TRACKED_HOURS = 'tracked hours'
-M30_INTERVAL_TRACKER = '30 minute interval tracker'
-CURRENT_WEEKDAY = 'current weekday'
-
-BASE5_OVERFLOW = 5
