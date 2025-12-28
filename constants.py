d304a2bbb3c89c75e44ea4894f518240ecc177f4
Sketched Doughnut <noreply>
2025-12-27 18:02:50 -0800

    making some changes, still works as should.

--- constants.py
+++ constants.py
@@ -1,5 +1,8 @@
 from os.path import dirname, abspath
 
+# at what point to increment the next thing
+# for example, overflow at 60 seconds to increase minutes by 1
+# overflow at 24 hours to increase days by 1
 BASE60_OVERFLOW = 60
 BASE24_OVERFLOW = 24
 BASE31_OVERFLOW = 31
@@ -8,27 +11,38 @@ BASE10_OVERFLOW = 10
 BASE5_OVERFLOW = 5
 BASE1_OVERFLOW = 1
 
-
+# all file paths
 DATA_FOLDERPATH = f'{dirname(abspath(__file__))}/screentime-local'
 DATA_FILEPATH = f'{DATA_FOLDERPATH}\\data.json'
 DAYLOG_FILEPATH = f'{DATA_FOLDERPATH}\\daylog.json'
 EXIT_FILEPATH = f'{DATA_FOLDERPATH}\\exit.txt'
 
+# making dictionary indexing easier
 START_TIMESTAMP = 'start timestamp'
-CURRENT_TIMESTAMP = 'current timestamp'
+ACCESS_TIMESTAMP = 'current timestamp'
 TRACKED_SECONDS = 'tracked seconds'
 TRACKED_MINUTES = 'tracked minutes'
 TRACKED_HOURS = 'tracked hours'
 SECOND_INTERVAL_TRACKER = '30 minute interval tracker'
 CURRENT_WEEKDAY = 'current weekday'
 
-# SUNDAY = 'sunday'
-# MONDAY = 'monday'
-# TUESDAY = 'tuesday'
-# WEDNESDAY = 'wednesday'
-# THURSDAY = 'thursday'
-# FRIDAY = 'friday'
-# SATURDAY = 'saturday'
+# defining all weekdays (and their number values)
+SUNDAY = 'sunday'
+MONDAY = 'monday'
+TUESDAY = 'tuesday'
+WEDNESDAY = 'wednesday'
+THURSDAY = 'thursday'
+FRIDAY = 'friday'
+SATURDAY = 'saturday'
+WEEKDAYS = {
+    0: MONDAY,
+    1: TUESDAY,
+    2: WEDNESDAY,
+    3: THURSDAY,
+    4: FRIDAY,
+    5: SATURDAY,
+    6: SUNDAY
+}
 
 # JANUARY = 'january'
 # FEBRUARY = 'february'
--- main.py
+++ main.py
@@ -18,19 +18,19 @@ class Screentime:
         # set up time tracker
         self.remembrance = {
             START_TIMESTAMP: self.get_timestamp(), # when the data.json was created
-            CURRENT_TIMESTAMP: self.get_timestamp(), # updates everytime the program saves data
-            TRACKED_SECONDS: 0, # tracks seconds past - usually set to zero besides when continuing on from previous values
-            TRACKED_MINUTES: 0, # tracks minutes past
-            TRACKED_HOURS: 0, # tracked hours pasted
-            SECOND_INTERVAL_TRACKER: 0, # interval counter for saving
+            ACCESS_TIMESTAMP: self.get_timestamp(), # updates everytime the program saves data
+            TRACKED_SECONDS: 0, # tracks seconds past START_TIMESTAMP - usually set to zero besides when continuing on via SecondSaveInterval
+            TRACKED_MINUTES: 0, # tracks minutes past START_TIMESTAMP
+            TRACKED_HOURS: 0, # tracked hours past START_TIMESTAMP
+            SECOND_INTERVAL_TRACKER: 0, # How many seconds between each save point
             CURRENT_WEEKDAY: datetime.datetime.weekday(datetime.datetime.now()) # tracks the current weekday, responsible for managing day overolls
         }
 
         # call functions
         self.load_data()
-        self.demise()
+        self.demise() # starts the neverending loop (until it ends)
 
-    # generate datetime timestamp
+    # generate timestamp via the datetime library
     def get_timestamp(self) -> str: return datetime.datetime.strftime(datetime.datetime.now(), "%d/%m/%Y, %H:%M:%S")
 
     # save data to a JSON file
@@ -48,25 +48,24 @@ class Screentime:
 
     # loads up data, does formatting
     def load_data(self):
-        data_state = os.path.exists(DATA_FILEPATH)
-        folder_state = os.path.exists(DATA_FOLDERPATH)
-        daylog_state = os.path.exists(DAYLOG_FILEPATH)
-        if not folder_state:
+        # if the project folder does not exist, try to make it; fail open
+        if not os.path.exists(DATA_FOLDERPATH):
             try: os.mkdir(DATA_FOLDERPATH)
             except: pass
-        # dump the dictionary made above into JSON
-        if not data_state: self.saveJSON(self.remembrance, DATA_FILEPATH)
-        # load JSON, update current timestamp, save new JSON
+        # if data JSON does not exist, create it with default values
+        if not os.path.exists(DATA_FILEPATH): self.saveJSON(self.remembrance, DATA_FILEPATH)
+        # load data JSON, set current timestamp, and save data JSON
         else:
             self.remembrance = self.loadJSON(DATA_FILEPATH)
-            self.remembrance[CURRENT_TIMESTAMP] = self.get_timestamp()
+            self.remembrance[ACCESS_TIMESTAMP] = self.get_timestamp()
             self.saveJSON(self.remembrance, DATA_FILEPATH)
-        if not daylog_state:
+        # if daylog JSON does not exist, create it with default values
+        if not os.path.exists(DAYLOG_FILEPATH):
             self.saveJSON({'nothing': 'nothing'}, DAYLOG_FILEPATH)
     
-    # saves remembrance
+    # saves the time keeping dictionary
     def save_timekeeper(self):
-        self.remembrance[CURRENT_TIMESTAMP] = self.get_timestamp() # update current timestamp
+        self.remembrance[ACCESS_TIMESTAMP] = self.get_timestamp() # update current timestamp
         self.saveJSON(self.remembrance, DATA_FILEPATH) # save to JSON
 
     # resets tracked measurements
@@ -77,8 +76,8 @@ class Screentime:
 
     # saves into daylog.json
     def save_daykeeper(self):
-        daykeeper = self.loadJSON(DAYLOG_FILEPATH) # load current daylog
-        daykeeper[self.remembrance[CURRENT_TIMESTAMP]] = self.remembrance # assign current remembrance to the current timestamp
+        daykeeper = self.loadJSON(DAYLOG_FILEPATH) # load current timekeeper
+        daykeeper[self.remembrance[ACCESS_TIMESTAMP]] = self.remembrance # assign current time keeper to the current timestamp
         self.saveJSON(daykeeper, DAYLOG_FILEPATH) # save new daylog to JSON
         self.remembrance[CURRENT_WEEKDAY] = datetime.datetime.weekday(datetime.datetime.now()) # update current weekday
         self.reset_timekeeper() # reset timekeeper
@@ -96,18 +95,19 @@ class Screentime:
                 # increase times, do rollovers
                 self.remembrance[TRACKED_SECONDS] += 1
                 self.remembrance[SECOND_INTERVAL_TRACKER] += 1
-                if self.remembrance[TRACKED_SECONDS] >= BASE60_OVERFLOW: #  overflow seconds to minute
+                if self.remembrance[TRACKED_SECONDS] >= BASE60_OVERFLOW: #  overflow seconds to minutes
                     self.remembrance[TRACKED_MINUTES] += 1
                     self.remembrance[TRACKED_SECONDS] = 0
-                if self.remembrance[TRACKED_MINUTES] >= BASE60_OVERFLOW: # overflow minutes to hour
+                if self.remembrance[TRACKED_MINUTES] >= BASE60_OVERFLOW: # overflow minutes to hours
                     self.remembrance[TRACKED_HOURS] += 1
                     self.remembrance[TRACKED_MINUTES] = 0
-                if self.remembrance[SECOND_INTERVAL_TRACKER] >= self.SecondSaveInterval: # save every (x) seconds
+                if self.remembrance[SECOND_INTERVAL_TRACKER] >= self.SecondSaveInterval: # save every (x) seconds, by default 60 seconds
                     self.remembrance[SECOND_INTERVAL_TRACKER] = 0 # reset interval counter
                     self.save_timekeeper() # save the timekeeper file to JSON
-                if self.debug: print(f'{self.remembrance[TRACKED_SECONDS]}s, {self.remembrance[TRACKED_MINUTES]}m, {self.remembrance[TRACKED_HOURS]}h, {self.remembrance[CURRENT_WEEKDAY]}, {datetime.datetime.weekday(datetime.datetime.now())}') # just printin
+                if self.debug: print(f'{self.remembrance[TRACKED_SECONDS]}s, {self.remembrance[TRACKED_MINUTES]}m, {self.remembrance[TRACKED_HOURS]}h, {self.remembrance[CURRENT_WEEKDAY]}, {WEEKDAYS[self.remembrance[CURRENT_WEEKDAY]]}') # just printin
                 time.sleep(1)
             except KeyboardInterrupt:
                 self.save_timekeeper()
                 exit()
+
 if __name__ == '__main__': Screentime()
